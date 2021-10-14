65ef78a92f6a54c69caed14601489e8bc0b7fcc4
Samyak Jain <samyakjainking@gmail.com>
2021-10-14 23:29:27 +0530

    11:29

--- models/index.js
+++ models/index.js
@@ -4,23 +4,14 @@ const fs = require('fs');
 const path = require('path');
 const Sequelize = require('sequelize');
 const basename = path.basename(__filename);
-const env = process.env.NODE_ENV || 'development';
+const env = process.env.NODE_ENV || 'production';
 const config = require(__dirname + '/../config/config.json')[env];
 const db = {};
 
 let sequelize;
 if (config.use_env_variable) {
   try{
-  sequelize = new Sequelize(process.env.DATABASE_URL, {
-    dialect: 'postgres',
-    protocol: 'postgres',
-    dialectOptions: {
-        ssl: {
-            require: true,
-            rejectUnauthorized: false
-        }
-    }
-});
+  sequelize = new Sequelize(process.env[config.use_env_variable], config);
     console.log(`DB ${process.env.database} CONNECTED`);
 }
 catch(err){
